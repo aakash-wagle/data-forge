76e961478fa69aa8d9510ec451b02009476375b1
unknown <aak.wagle@gmail.com>
2023-03-23 04:19:44 +0530

    Adding Axios

---
+++ src/api/index.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+const API = axios.create({ baseURL: "http://localhost:5000" });
+
+export const fetchPosts = () => API.get("/posts");
+export const createPost = (newPost) => API.post("/posts", newPost);
+export const updatePost = (id, updatedPost) =>
+  API.patch(`/posts/${id}`, updatedPost);
+export const deletePost = (id) => API.delete(`/posts/${id}`);
+export const likePost = (id, updatedPost) =>
+  API.patch(`/posts/${id}/like`, updatedPost);
+
+// FI: Auth requests
+// export const signIn = (formData) => API.post("/user/signin", formData);
+// export const signUp = (formData) => API.post("/user/signup", formData);
