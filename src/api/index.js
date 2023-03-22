4267640808ab6fe9359e6b8ed874c7e6bf18721a
unknown <aak.wagle@gmail.com>
2023-03-23 05:01:54 +0530

    Refactoring API calls

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,25 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "" });
+const API = axios.create({ baseURL: "https://dataforge.onrender.com/" });
 
+// export const fetchPosts = () => API.get("/posts");
+// export const createPost = (newPost) => API.post("/posts", newPost);
+// export const updatePost = (id, updatedPost) =>
+//   API.patch(`/posts/${id}`, updatedPost);
+// export const deletePost = (id) => API.delete(`/posts/${id}`);
+// export const likePost = (id, updatedPost) =>
+//   API.patch(`/posts/${id}/like`, updatedPost);
 
 // FI: Auth requests
-// export const signIn = (formData) => API.post("/user/signin", formData);
-// export const signUp = (formData) => API.post("/user/signup", formData);
+export const login = (formData) =>
+  API.post("/login", formData, {
+    headers: { "Content-Type": "application/json" },
+  });
+
+export const register = (formData) =>{
+  console.log("Register API called");
+  return API.post("/register", formData, {
+    headers: { "Content-Type": "application/json" },
+  })};
+
+// export const signUp = (formData) => API.post("/user/", formData);
--- src/components/Navbar/Login.jsx
+++ src/components/Navbar/Login.jsx
@@ -1,174 +1,184 @@
-import { useState } from "react"
+import { useState } from "react";
 import { Fragment } from "react";
-import { Modal,Box } from "@mui/material";
-import style from "./Login.module.css"
-import axios from 'axios';
+import { Modal, Box } from "@mui/material";
+import style from "./Login.module.css";
+import axios from "axios";
 // import { makeStyles } from '@material-ui/core/styles';
-import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
 import { display } from "@mui/system";
+import * as api from "../../api";
 
+export const LoginModal = (props) => {
+  const [loginForm, setLoginForm] = useState({
+    loginemail: "",
+    loginpassword: "",
+  });
 
+  const [signupForm, setSignupForm] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
 
-export const LoginModal = (props)=>{
-    const [loginForm, setLoginForm] = useState({
-        loginemail: '',
-        loginpassword: '',
+  const [login, setLogin] = useState(true);
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+
+    if (name === "loginemail" || name === "loginpassword") {
+      setLoginForm((prevState) => ({ ...prevState, [name]: value }));
+    } else {
+      setSignupForm((prevState) => ({ ...prevState, [name]: value }));
+    }
+  };
+
+  const handleLoginSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const res = await api.login({
+        email: loginForm.loginemail,
+        password: loginForm.loginpassword,
       });
-      
-    const [signupForm, setSignupForm] = useState({
-        username: '',
-        email: '',
-        password: '',
-    });
-
-    const [login, setLogin] = useState(true);
-
-    const handleInputChange = (event) => {
-        const { name, value } = event.target;
-      
-        if (name === 'loginemail' || name === 'loginpassword') {
-          setLoginForm((prevState) => ({ ...prevState, [name]: value }));
-        } else {
-          setSignupForm((prevState) => ({ ...prevState, [name]: value }));
-        }
+      console.log(res);
+      // Handle successful login
+      const userObj = {
+        token: res.token,
+        user: res.user,
       };
+      localStorage.setItem("User", userObj);
+    } catch (error) {
+      // Handle login error
+      console.log(error);
+    }
+  };
 
-    const handleLoginSubmit = async (event) => {
-        event.preventDefault();
-        
-        try {
-            const res = await axios.post('https://dataforge.onrender.com/login', {
-               
-                email: loginForm.loginemail,
-                password: loginForm.loginpassword
-            },
-            {
-                headers: {'Content-Type': 'application/json'}
-            }).then((response)=>{
-                  console.log(response);
-                  const userObj = {
-                    token: response.token,
-                    user: response.user
-                  }
-                  localStorage.setItem('User',userObj);
-              }
-            );
-        
-            // Handle successful login
-            } catch (error) {
-                // Handle login error
-                console.log(error);
-            }
-    };
-    
-    const handleSignupSubmit = async (event) => {
-        event.preventDefault();
-        
-        try {
-            const res = await axios.post('https://dataforge.onrender.com/register', {
-                username: signupForm.username,
-                email: signupForm.email,
-                password: signupForm.password
-            },
-            {
-                headers: {'Content-Type': 'application/json'}
-            }).then((response)=>{
-                  console.log(response);
-                  if(response.status==200){
+  const handleSignupSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const res = await api.register({
+        username: signupForm.username,
+        email: signupForm.email,
+        password: signupForm.password,
+      });
+      console.log(res);
+      // Handle successful signup
+      if (res.status == 200) {
+        const userObj = {
+          token: res.token,
+          user: res,
+          // user, 
+        };
+        localStorage.setItem("User", userObj);
+      }
 
-                      const userObj = {
-                        token: response.token,
-                        user: response,user
-                      }
-                      localStorage.setItem('User',userObj);
-                  }
-              }
-            );
-        
-            // Handle successful signup
-            } catch (error) {
-                // Handle signup error
-                console.log(error);
-            }
-    };
+    } catch (error) {
+      // Handle signup error
+      console.log(error);
+    }
+  };
 
-    return(
-        <Fragment>
-            <Modal
-                open={props.openLogin}
-                onClose={()=>{props.setOpenLogin(false)}}
-                // open={setOpen(true)}
-                // onClose={setOpen(false)}
-                aria-labelledby="modal-modal-title"
-                aria-describedby="modal-modal-description"
-                
-            >
-                <Box className={style.box} >
-                    <div className={style.selectionContainer}>
-                        <div className={style.selection}>
-                            <Button onClick={()=>{setLogin(true)}} style={{fontWeight:login? 'bold' : 'normal'}}>Login</Button>
-                            <Button onClick={()=>{setLogin(false)}} style={{fontWeight:!login? 'bold' : 'normal'}}>Sign Up</Button>
-                        </div>
-                        {login?
-
-                        <div className={style.loginForm}>
-                            <form className={style.form} noValidate autoComplete="off" onSubmit={handleLoginSubmit}>
-                                <TextField
-                                    id="loginemail"
-                                    label="Email"
-                                    name="loginemail"
-                                    value={loginForm.email}
-                                    onChange={handleInputChange}
-                                />
-                                <TextField
-                                    id="loginpassword"
-                                    label="Password"
-                                    name="loginpassword"
-                                    value={loginForm.password}
-                                    onChange={handleInputChange}
-                                />
-                                <Button type="submit" variant="contained" color="primary">
-                                    Login
-                                </Button>
-                            </form>
-
-                        </div>:
-
-                        <div className={style.signupForm}>
-                            <form className={style.form} noValidate autoComplete="off" onSubmit={handleSignupSubmit}>
-                                <TextField
-                                        id="username"
-                                        label="Username"
-                                        name="username"
-                                        value={signupForm.username}
-                                        onChange={handleInputChange}
-                                />
-                                <TextField
-                                    id="email"
-                                    label="Email"
-                                    name="email"
-                                    value={signupForm.email}
-                                    onChange={handleInputChange}
-                                />
-                                <TextField
-                                    id="password"
-                                    label="Password"
-                                    name="password"
-                                    value={signupForm.password}
-                                    onChange={handleInputChange}
-                                />
-                                <Button type="submit" variant="contained" color="primary">
-                                    Sign up
-                                </Button>
-                            </form>
-                        </div>
-                        }
-                    </div>
-                <Button>Google login</Button>
-                </Box>
-            </Modal>
-        </Fragment>
-    );
-
-}
+  return (
+    <Fragment>
+      <Modal
+      /* eslint-disable react/prop-types */
+        open={props.openLogin}
+        onClose={() => {
+          props.setOpenLogin(false);
+        }}
+        // open={setOpen(true)}
+        // onClose={setOpen(false)}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box className={style.box}>
+          <div className={style.selectionContainer}>
+            <div className={style.selection}>
+              <Button
+                onClick={() => {
+                  setLogin(true);
+                }}
+                style={{ fontWeight: login ? "bold" : "normal" }}
+              >
+                Login
+              </Button>
+              <Button
+                onClick={() => {
+                  setLogin(false);
+                }}
+                style={{ fontWeight: !login ? "bold" : "normal" }}
+              >
+                Sign Up
+              </Button>
+            </div>
+            {login ? (
+              <div className={style.loginForm}>
+                <form
+                  className={style.form}
+                  noValidate
+                  autoComplete="off"
+                  onSubmit={handleLoginSubmit}
+                >
+                  <TextField
+                    id="loginemail"
+                    label="Email"
+                    name="loginemail"
+                    value={loginForm.email}
+                    onChange={handleInputChange}
+                  />
+                  <TextField
+                    id="loginpassword"
+                    label="Password"
+                    name="loginpassword"
+                    type="password"
+                    value={loginForm.password}
+                    onChange={handleInputChange}
+                  />
+                  <Button type="submit" variant="contained" color="primary">
+                    Login
+                  </Button>
+                </form>
+              </div>
+            ) : (
+              <div className={style.signupForm}>
+                <form
+                  className={style.form}
+                  noValidate
+                  autoComplete="off"
+                  onSubmit={handleSignupSubmit}
+                >
+                  <TextField
+                    id="username"
+                    label="Username"
+                    name="username"
+                    value={signupForm.username}
+                    onChange={handleInputChange}
+                  />
+                  <TextField
+                    id="email"
+                    label="Email"
+                    name="email"
+                    value={signupForm.email}
+                    onChange={handleInputChange}
+                  />
+                  <TextField
+                    id="password"
+                    label="Password"
+                    name="password"
+                    type="password"
+                    value={signupForm.password}
+                    onChange={handleInputChange}
+                  />
+                  <Button type="submit" variant="contained" color="primary">
+                    Sign up
+                  </Button>
+                </form>
+              </div>
+            )}
+          </div>
+          <Button>Google login</Button>
+        </Box>
+      </Modal>
+    </Fragment>
+  );
+};
